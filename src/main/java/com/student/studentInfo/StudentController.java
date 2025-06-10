a32c3cfe14cc3026adf1dd9a14016ea2e50f193b
Om <mynameisompandey007@gmail.com>
2025-06-10 10:43:52 +0530

    Service layer added

--- src/main/java/com/student/studentInfo/StudentController.java
+++ src/main/java/com/student/studentInfo/StudentController.java
@@ -3,6 +3,7 @@ package com.student.studentInfo;
 
 import com.student.studentInfo.model.StudentModel;
 import com.student.studentInfo.repository.StudentRepository;
+import com.student.studentInfo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -22,6 +23,9 @@ public class StudentController {
     @Autowired
   private  StudentRepository studentRepository;
 
+    @Autowired
+    private StudentService studentService;
+
     @GetMapping(path = "getAll")
     public List<StudentModel> getAllStudents(){
         return  studentRepository.findAll();
@@ -37,52 +41,44 @@ public class StudentController {
 
     @PostMapping(path = "add")
     public StudentModel addStudent(@RequestBody StudentModel student){
-      StudentModel  newStudent =  studentRepository.save(student);
-        return newStudent;
+
+        return studentService.addStudent(student);
     }
 
     @PutMapping(path = "update/{id}")
     public ResponseEntity<String> updateStudent(@PathVariable Long id,@RequestBody StudentModel updatedStudent){
-      Optional<StudentModel> existingStudentOptional=studentRepository.findById(id);
 
-      if(existingStudentOptional.isPresent()){
-        StudentModel existingStudent=existingStudentOptional.get();
-        existingStudent.setName(updatedStudent.getName());
-        existingStudent.setAge(updatedStudent.getAge());
-        existingStudent.setEmail(updatedStudent.getEmail());
-        existingStudent.setCourse(updatedStudent.getCourse());
-        studentRepository.save(existingStudent);
-        return ResponseEntity.ok("The student with ID:"+existingStudent.getId()+" and Name: "+existingStudent.getName() +" Has been Successfully Updated");
-      }else{
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The Student with ID: "+ id+" Not found");
-      }
+        StudentModel updated= studentService.updateStudent(id,updatedStudent);
+         return ResponseEntity.ok("The student with ID:"+updated.getId()+" and Name: "+updated.getName() +" Has been Successfully Updated");
+//      Optional<StudentModel> existingStudentOptional=studentRepository.findById(id);
+//      if(existingStudentOptional.isPresent()){
+//        StudentModel existingStudent=existingStudentOptional.get();
+//        existingStudent.setName(updatedStudent.getName());
+//        existingStudent.setAge(updatedStudent.getAge());
+//        existingStudent.setEmail(updatedStudent.getEmail());
+//        existingStudent.setCourse(updatedStudent.getCourse());
+//        studentRepository.save(existingStudent);
+//        return ResponseEntity.ok("The student with ID:"+existingStudent.getId()+" and Name: "+existingStudent.getName() +" Has been Successfully Updated");
+//      }else{
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The Student with ID: "+ id+" Not found");
+//      }
     }
 
 
 
     @DeleteMapping(path = "delete/{id}")
         public ResponseEntity<String> deleteStudent(@PathVariable Long id){
-      Optional<StudentModel> student= studentRepository.findById(id);
-             if(student.isPresent()){
-             studentRepository.deleteById(id);
-             return ResponseEntity.ok("student with ID "+id+"has been deleted");
-             }
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error: Student with id= "+id+"not found");
 
+      return studentService.deleteStudent(id);
         }
 
         @DeleteMapping(path = "deleteAll")
        public String deleteAll(){
-      try {
-         studentRepository.deleteAll();
-        return "Deleted all Records Successfully";
-      }catch (Exception e){
-        return "Something Went Wrong"+e;
-      }
-
-
+       return studentService.deleteAll();
 
         }
+
+
 
 }
 
---
+++ src/main/java/com/student/studentInfo/service/StudentService.java
@@ -0,0 +1,70 @@
+package com.student.studentInfo.service;
+
+import com.student.studentInfo.model.StudentModel;
+import com.student.studentInfo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.rmi.StubNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public List<StudentModel> getAllStudents(){
+        return  studentRepository.findAll();
+    }
+
+    public StudentModel getStudent(Long id){
+        return studentRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Student with ID:"+id+ "not found"));
+    }
+
+    public StudentModel addStudent(StudentModel student){
+        StudentModel  newStudent =  studentRepository.save(student);
+        return newStudent;
+    }
+
+    public StudentModel updateStudent(Long id,StudentModel updatedStudent){
+        StudentModel existingStudent=studentRepository.findById(id)
+        .orElseThrow(()->new RuntimeException("Student with id: "+id+"not found"));
+
+
+            existingStudent.setName(updatedStudent.getName());
+            existingStudent.setAge(updatedStudent.getAge());
+            existingStudent.setEmail(updatedStudent.getEmail());
+            existingStudent.setCourse(updatedStudent.getCourse());
+
+        return    studentRepository.save(existingStudent);
+
+
+    }
+
+    public ResponseEntity<String> deleteStudent(Long id) {
+
+        Optional<StudentModel> student= studentRepository.findById(id);
+        if(student.isPresent()){
+            studentRepository.deleteById(id);
+            return ResponseEntity.ok("student with ID "+id+"has been deleted");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error: Student with id= "+id+"not found");
+    }
+
+    public String deleteAll(){
+        try {
+            studentRepository.deleteAll();
+            return "Deleted all Records Successfully";
+        }catch (Exception e){
+            return "Something Went Wrong"+e;
+        }
+      }
+
+}
